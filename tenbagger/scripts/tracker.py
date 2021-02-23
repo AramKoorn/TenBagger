d3097e9c55c9fb61786c03497dd7f4aa64f7498a
aram <aram.koorn@icloud.com>
2021-02-23 23:39:20 +0100

    start with formatting data for tracker dashboard

---
+++ tenbagger/scripts/tracker.py
@@ -0,0 +1,37 @@
+import pandas as pd
+import numpy as np 
+import yfinance as yf
+import yaml
+
+
+def main(config):
+
+    total = []
+
+    for sector in config:
+        s = config[sector]
+
+        res = []
+        for ind, ticker in s.items():
+            t = yf.Ticker(ticker)
+            info = t.info
+
+            op = info["open"]
+            close = info["previousClose"]
+            change = (close - op) / op * 100
+            val = [ind, change]
+            res.append(val)
+
+        df = pd.DataFrame(data=res, columns=[sector, sector + "Change"])
+        total.append(df)
+
+    return pd.concat(total, axis=1)
+
+
+if __name__ == "__main__":
+    
+    with open(r'configs/trackers.yaml') as file:
+        config = yaml.load(file, Loader=yaml.FullLoader)
+
+    df = main(config)
+    print(df)
