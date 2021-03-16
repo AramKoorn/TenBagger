98ffc6eead0a1f6f2bc8c9c5c6219caf13da21f7
AramKoorn <aram.koorn@icloud.com>
2021-03-17 00:08:38 +0100

    asdfjaskldf

--- tenbagger/scripts/insiders_overview.py
+++ tenbagger/scripts/insiders_overview.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 
 
-def clean_insider(col):
+def clean_insider(df, col):
 
     df[col] = df[col].str.replace('+', '')
     df[col] = df[col].str.replace(',', '')
@@ -10,14 +10,18 @@ def clean_insider(col):
 
     df[col] = df[col].astype('float')
 
+    return df
+
 
 def overview():
 
-    x = 2
     pd.set_option("expand_frame_repr", False)
 
     df = pd.read_html('http://openinsider.com/')[10]
     clean_cols = ['Qty', 'Price', 'Value']
+
+    for col in clean_cols:
+        df = clean_insider(df, col)
 
     pass
 
