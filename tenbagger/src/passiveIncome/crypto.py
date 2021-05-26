7dcd6cc5d3af570e8f46c426abc0b4ed96846ec2
AramKoorn <aram.koorn@icloud.com>
2021-05-26 23:11:09 +0200

    include crypto staking rewards to calculator

--- tenbagger/src/passiveIncome/crypto.py
+++ tenbagger/src/passiveIncome/crypto.py
@@ -1,2 +1,7 @@
-class StakingRewards:
+from tenbagger.src.passiveIncome.calculator import PassiveIncomeCalculator
+
+
+class StakingRewards(PassiveIncomeCalculator):
+    def __init__(self, port):
+        super().__init__(port=port)
     pass
--- test/passiveIncome/test_calculator.py
+++ test/passiveIncome/test_calculator.py
@@ -10,15 +10,27 @@ class TestCalculator:
 
     def test_calculator_only_div(self):
         # df = self.ca
-        df = self.calc.calulate_dividends(n=120, growth_stock=0.03, growth_dividend=0.03, monthly_payment=1000,
+        df = self.calc.calulate(n=120, growth_stock=0.03, growth_dividend=0.03, monthly_payment=1000,
                                           generate_report=False, only_dividend_stocks=True)
         assert set(df.ticker) == set(['aapl'])
 
     def test_calculator_mixed(self):
-        df = self.calc.calulate_dividends(n=120, growth_stock=0.03, growth_dividend=0.03, monthly_payment=1000,
+        df = self.calc.calulate(n=120, growth_stock=0.03, growth_dividend=0.03, monthly_payment=1000,
                                           generate_report=False, only_dividend_stocks=False)
         assert set(df.ticker) == set(['eth-usd', 'aapl'])
 
     def test_passive_mixed(self):
-        df = self.calc.calulate_dividends(n=120, growth_stock=0.03, growth_dividend=0.03, monthly_payment=1000,
+        df = self.calc.calulate(n=120, growth_stock=0.03, growth_dividend=0.03, monthly_payment=1000,
                                           generate_report=False, only_dividend_stocks=False)
+
+
+class TestCalculator:
+    def setup(self):
+
+        pd.set_option('expand_frame_repr', False)
+        self.calc = PassiveIncomeCalculator(port='test_calculator2')
+
+    def test_calculator_multi(self):
+        # df = self.ca
+        df = self.calc.calulate(n=120, growth_stock=0.03, growth_dividend=0.03, monthly_payment=1000,
+                                generate_report=True, only_dividend_stocks=False)
